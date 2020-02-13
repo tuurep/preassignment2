129fbc3998134db4ff0d3d4a0f8d2f79d916f9e5
tuure <tuure.piitulainen@gmail.com>
2020-02-13 15:35:58 +0200

    Show file contents on HTML page

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from bottle import route, run
+import numpy as np
 
 @route('/hello')
 def hello():
-    return "Hello World!"
+  return "Hello World!"
+
+@route('/file_dump')
+def file_dump():
+  f = open("statusfile.txt", "r")
+  lines = f.readlines()
+  return lines
 
 run(host='localhost', port=8080, debug=True)

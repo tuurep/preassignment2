3c49c567d2725a353418bacbec3d56be339391fa
tuure <tuure.piitulainen@gmail.com>
2020-02-16 00:32:26 +0200

    Route to HTML templates

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ from bottle import route, run, template
 
 def parse_file(input):
   f = open(input, "r")
-  keywords = ('Package:', 'Description:', "Depends:")
   lines = []
 
   for line in f.readlines():
-    if line.startswith(keywords):
-      lines.append('<p>{}</p>'.format(line))
+    if line.startswith('Package:'):
+      lines.append(str.split(line)[1])
 
   f.close()
-  return lines
+
+  return sorted(lines)
 
 @route('/')
 def index():
-  return parse_file("statusfile.txt")
+  return template('index', pkg_names=parse_file('statusfile.txt'))
+
+@route('/<pkg_name>')
+def info(pkg_name):
+  return template('pkg_info', pkg_name=pkg_name)
 
 run(host='localhost', port=8080, debug=True, reloader=True)

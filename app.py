3bf0155628f8c4329413707462a7d7a9e8e15ace
tuure <tuure.piitulainen@gmail.com>
2020-02-16 23:00:56 +0200

    Separate text parsing and routing to different files

--- app.py
+++ app.py
@@ -1,74 +1,9 @@
 from bottle import route, run, template
-
-def add_reverse_dependencies(pkgs):
-  for pkg in pkgs:
-    for dep in pkgs[pkg]["dependencies"]:
-      if dep in pkgs:
-        pkgs[dep]["reverse_dependencies"].append(pkg)
-
-def without_field_header(line):
-  # Returns line without the first word
-  return line.split(' ', 1)[1].strip()
-
-def cut_version_numbers(dep_list):
-  without_ver = []
-
-  for dep in dep_list:
-    without_ver.append(str.split(dep)[0])
-
-  return without_ver
-
-def file_to_dict(input):
-  pkgs = {}
-
-  concat_multiliner = False
-  d_paragraph = ''
-  current_key = ''
-
-  f = open(input, "r")
-  
-  for line in f.readlines():
-    if line.startswith('Package:'):
-      current_key = without_field_header(line)
-      pkgs[current_key] = {
-                            "description": [],
-                            "dependencies": [],
-                            "reverse_dependencies": []
-                          }
-
-    if line.startswith('Depends:'):
-      list_as_str = without_field_header(line)
-      dep_list = list_as_str.split(', ')
-      dep_list = cut_version_numbers(dep_list)
-      pkgs[current_key]["dependencies"] = dep_list
-
-    if concat_multiliner:
-      if line.startswith(' .'):
-        pkgs[current_key]["description"].append(d_paragraph)
-        d_paragraph = ''
-      
-      elif line.startswith(('Original-Maintainer', 'Homepage:')):
-        pkgs[current_key]["description"].append(d_paragraph)
-        d_paragraph = ''
-        concat_multiliner = False
-      
-      else:
-        d_paragraph += line
-        
-    if line.startswith('Description:'):
-      d_paragraph = without_field_header(line)
-      concat_multiliner = True
-  
-  f.close()
-
-  add_reverse_dependencies(pkgs)
-
-  return pkgs
-
+from fileparser import file_to_dict
 
 @route('/')
 def index():
-  pkgs=file_to_dict('statusfile.txt')
+  pkgs = file_to_dict('statusfile.txt')
 
   pkgs_arr = []
   for pkg in pkgs:
@@ -81,7 +16,7 @@ def info(pkg_name):
   pkgs = file_to_dict('statusfile.txt')
 
   if pkg_name not in pkgs:
-    return '''<p>Package {} was not found</p>
+    return '''<h3>Package {} was not found</h3>
               <a href="/">Back to index</a>'''.format(pkg_name)
 
   return template('pkg_info', pkg_name=pkg_name, pkg=pkgs[pkg_name])
---
+++ fileparser.py
@@ -0,0 +1,65 @@
+def add_reverse_dependencies(pkgs):
+  for pkg in pkgs:
+    for dep in pkgs[pkg]["dependencies"]:
+      if dep in pkgs:
+        pkgs[dep]["reverse_dependencies"].append(pkg)
+
+def without_field_header(line):
+  # Returns line without the first word
+  return line.split(' ', 1)[1].strip()
+
+def cut_version_numbers(dep_list):
+  without_ver = []
+
+  for dep in dep_list:
+    without_ver.append(str.split(dep)[0])
+
+  return without_ver
+
+def file_to_dict(input):
+  pkgs = {}
+
+  concat_multiliner = False
+  d_paragraph = ''
+  current_key = ''
+
+  f = open(input, "r")
+  
+  for line in f.readlines():
+    if line.startswith('Package:'):
+      current_key = without_field_header(line)
+      pkgs[current_key] = {
+                            "description": [],
+                            "dependencies": [],
+                            "reverse_dependencies": []
+                          }
+
+    if line.startswith('Depends:'):
+      list_as_str = without_field_header(line)
+      dep_list = list_as_str.split(', ')
+      dep_list = cut_version_numbers(dep_list)
+      pkgs[current_key]["dependencies"] = dep_list
+
+    if concat_multiliner:
+      if line.startswith(' .'):
+        pkgs[current_key]["description"].append(d_paragraph)
+        d_paragraph = ''
+      
+      elif line.startswith(('Original-Maintainer', 'Homepage:')):
+        pkgs[current_key]["description"].append(d_paragraph)
+        d_paragraph = ''
+        concat_multiliner = False
+      
+      else:
+        d_paragraph += line
+        
+    if line.startswith('Description:'):
+      d_paragraph = without_field_header(line)
+      concat_multiliner = True
+  
+  f.close()
+
+  add_reverse_dependencies(pkgs)
+
+  return pkgs
+

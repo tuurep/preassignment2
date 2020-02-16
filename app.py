548709e7e996289f3aa4889925ebbdf13a8fbbf2
tuure <tuure.piitulainen@gmail.com>
2020-02-16 17:57:17 +0200

    Parse multiliner fields

--- app.py
+++ app.py
@@ -1,21 +1,52 @@
 from bottle import route, run, template
 
-def parse_file(input):
-  f = open(input, "r")
-  lines = []
+def filter_fields(input):
+  fields_as_str = []
 
+  oneliner = ('Package:', 'Depends:')
+  multiliner = ('Description:')
+  description_end = ('Original-Maintainer', 'Homepage:')
+  concat_multiliner = False
+  multiline_str = ''
+
+  f = open(input, "r")
+  
   for line in f.readlines():
-    if line.startswith('Package:'):
-      after_whitespace = str.split(line)[1]
-      lines.append(after_whitespace)
+    if line.startswith(oneliner):
+      fields_as_str.append('<p>{}</p>'.format(line))
+
+    if line.startswith(multiliner):
+      multiline_str = '<p>'
+      concat_multiliner = True
+
+    if concat_multiliner:
+      if line.startswith(' .'):
+        multiline_str += '</p><p>'
+      
+      elif not line.startswith(description_end):
+        multiline_str += line
+        
+      else:
+        fields_as_str.append(multiline_str + '</p>')
+        multiline_str = ''
+        concat_multiliner = False
 
   f.close()
 
-  return sorted(lines)
+  return fields_as_str
+
+def file_to_dict(input):
+  packages = {}
+
+  for field in filter_fields(input):
+    pass
+
+  return packages
 
 @route('/')
 def index():
-  return template('index', pkg_names=parse_file('statusfile.txt'))
+  return filter_fields('statusfile.txt')
+  # return template('index', packages=file_to_dict('statusfile.txt'))
 
 @route('/<pkg_name>')
 def info(pkg_name):
